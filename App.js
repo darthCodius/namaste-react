585ec91020322212cd17cca77595071483651da2
Abhinav Yadav <abhiyadavpky@gmail.com>
2024-05-18 15:29:02 +0530

    done

--- App.js
+++ App.js
@@ -1,16 +1,90 @@
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1"),
-    React.createElement("h2", {}, "I am h2"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1"),
-    React.createElement("h2", {}, "I am h2"),
-  ]),
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello React!");
+/**
+ * Header
+ *  - Logo
+ *  - Nav
+ *
+ * Body
+ *  -Search
+ *  - RestaurantContainer
+ *     -RestaurantCard
+ *         -Image
+ *         -Name of Restaurant, Star Rating, Cuisine, Delivery Time
+ *
+ *
+ *  Footer
+ *      -Copyright
+ *      -Links
+ *      -Address
+ *      -Contact
+ */
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://graphicsfamily.com/wp-content/uploads/edd/2021/06/Editable-Photoshop-Food-Logo-Design-PNG-Transparent-2048x2048.png"
+        />
+      </div>
+
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/0cd4d1e98a6476d83437ac2684db5d68"
+        alt="res-logo"
+        className="res-logo"
+      />
+      <h3>Meghana Foods</h3>
+      <h5>Chinese, Indo-Chinese</h5>
+      <h5>4.3⭐</h5>
+      <h5>38 Mins</h5>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<AppLayout />);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-#Namaste React 🚀
+# Namaste React 🚀
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algo - written in C++
+- Caching hence Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashinh
+- Code Splitting
+- Differential Bundlingc
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree
